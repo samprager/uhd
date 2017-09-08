4fa3196152a1dfd2f340a64b70970ed2248414f4
samprager <sprager@usc.edu>
2017-09-08 09:41:25 -0700

    gain tables

---
+++ tools/matlab/freqstack_compress.m
@@ -0,0 +1,147 @@
+function varargout = freqstack_compress(varargin)
+% freqstack - Synthesize wide band chirp signal with frequency stacking and
+% compress pulse
+%
+% Usage:
+%    [data_u, filt_u, (x_tx), (upfac)] = freqstack_compress(trials)
+%    [data_u, filt_u, (x_tx), (upfac)] = freqstack_compress(trials,Bs)
+%    [data_u, filt_u, (x_tx), (upfac)] = freqstack_compress(trials,dfc,Bs,upfac)
+%    [data_u, filt_u, (x_tx), (upfac)] = freqstack_compress(trials,dfc,Bs,upfac,fs)
+% Inputs:
+%    trials - struct array of trials. Must contain field trials.data,
+%    trials.ref, trials.awglen, and trials.fs
+%    upfac - upsample factor
+%    dfc - center frequency separation
+%    Bs - sub waveform bandwidth
+%    fs - sample freq
+%
+% Outputs:
+%    data_u - compressed pulse after freq stacking
+%    l - sample lag
+%    d_tx - ideal wideband compressed pulse
+%    upfac - upsampling factor
+
+% See also: udar_read.m
+
+% Author: Samuel Prager
+% University of Southern California
+% email: sprager@usc.edu
+% Created: 2017/04/06 01:13:06; Last Revised: 2017/04/06 01:13:06
+
+%------------- BEGIN CODE --------------
+if(nargin==1)
+    trials = varargin{1}; 
+    fs = trials(1).fs;
+    N = numel(trials);
+    if (N>1)
+        dfc = trials(2).freq-trials(1).freq; 
+    else
+        error('When using one argument, trials struct array must contain at least two trials'); 
+    end
+    Bs = dfc;
+    upfac = ceil(N*Bs/fs);   
+elseif(nargin==2)
+    trials = varargin{1};
+    Bs = varargin{2}; 
+    fs = trials(1).fs;
+    N = numel(trials);
+    if (N>1)
+        dfc = trials(2).freq-trials(1).freq; 
+    else
+        dfc = Bs;
+    end
+    upfac = ceil(N*Bs/fs);    
+elseif(nargin==4)
+    trials = varargin{1};
+    fs = trials(1).fs;
+    dfc = varargin{2};
+    Bs = varargin{3};
+    N = numel(trials);
+    upfac = varargin{4};
+elseif(nargin==5)
+    trials = varargin{1};
+    dfc = varargin{2};
+    Bs = varargin{3};
+    N = numel(trials);
+    upfac = varargin{4};
+    fs = varargin{5};
+else
+    usage = sprintf('\n\t[data_u, filt_u, (x_tx)] = freqstack_compress(trials,dfc)\n\t[data_u, filt_u, (x_tx)] = freqstack_compress(trials,dfc,Bs,upfac)\n\t[data_u, filt_u, (x_tx)] = freqstack_compress(trials,dfc,Bs,upfac,fs)');
+    error(['Invalid number of arguments. Usage: ',usage]);
+end
+    
+if ((numel(trials(1).ref)==0) || (fs==0) || (~isstruct(trials)))
+    error('trials must be a struct with fields trials.data,trials.ref, trials.awglen, (and trials.fs)');
+end
+    
+% n = trials(1).awglen*upfac;
+n= numel(trials(1).data)*upfac;
+fs2 = fs*upfac;
+Tp = n/fs2;
+K = Bs/Tp;
+fnq = N*Bs*2;
+fc0 = 0; 
+if (nargin>2)
+    fc = fc0+(N-1)*dfc/2;
+    fn = fc0+(0:(N-1))*dfc;
+else
+    fc = (trials(end).freq+trials(1).freq)/2;
+    fn = [trials.freq];
+end
+dfn = fn-fc;
+dTn = dfn/K;
+
+
+nzero = (upfac-1)*numel(trials(1).data)/2;
+
+ntau = upfac*numel(trials(1).data)-n; 
+tau=ntau/fs2;
+
+D = zeros(N,upfac*numel(trials(1).data));
+
+for i=1:N
+    dfft = fftshift(fft(trials(i).data).*conj(fft(trials(i).ref)));
+    ftemp = linspace(-fs/2,fs/2,numel(dfft));
+    
+    flim1 = -Bs/2;
+    flim2 = Bs/2;
+    if (i==1)
+        flim1 = ftemp(1);
+    end
+    if(i==N)
+        flim2 = ftemp(end);
+    end
+    dfft = dfft.*rect(ftemp,flim1,flim2,1);
+
+    dfft = [zeros(1,nzero),dfft,zeros(1,nzero)];
+    D(i,:) = shift(dfft,dfn(i)*numel(dfft)/fs2);
+
+end
+
+data_u = fftshift(ifft(ifftshift(sum(D))));
+
+t_tx = linspace(-Tp/2,Tp/2+tau,numel(data_u));
+% x_tx = exp(1i*pi*K*t_tx.^2).*rect(t_tx,t_tx(1),t_tx(end-ntau),1);
+x_tx = exp(1i*pi*((dfn(end)+Bs/2)/(Tp/2))*t_tx.^2).*rect(t_tx,t_tx(1),t_tx(end-ntau),1);
+
+d_tx = fftshift(ifft(fft(x_tx).*conj(fft(x_tx))));
+
+
+l = linspace(-numel(data_u)/2,numel(data_u)/2,numel(data_u));
+l = l + l(end/2);
+
+ 
+if(nargout>=1)
+    varargout{1}=data_u;
+end
+if(nargout>=2)
+    varargout{2}=l;
+end
+if(nargout>=3)
+    varargout{3}=d_tx;
+end
+if(nargout>=4)
+    varargout{4}=upfac;
+end
+end
+%------------- END OF CODE --------------
--- tools/matlab/gaintable.m
+++ tools/matlab/gaintable.m
@@ -1,10 +1,9 @@
 
-addpath 'USCRepos\uhd\tools\matlab'
-sharedir = 'C:\Users\prager\VirtualBox VMs\share_folder';
+addpath '~/Ettus/uhd/tools/matlab';
 
 fs = 5e7;
 
-fmin = 0e9; fmax = 4e9;
+fmin = 1e9; fmax = 4e9;
 
 dfc = 45e6; Bs = 45e6;
 er = 1;
@@ -13,23 +12,34 @@ v = 3e8/sqrt(er);
 rngint = linspace(-100,400,10000);
 
 %extampfullsweep extamp_gainluts extamp_txagc
-trialname = 'extamp_txagc2';
-outdir = [sharedir,'\outputs\',trialname];
-wavedir = [sharedir,'\waveforms\'];
+%extamp_txgains_min26_ant_limited_gain40 extamp_txgains_min26_ant_default
+trialname = 'extamp_txgains_min26_ant_default';
+outdir = ['../outputs/',trialname];
+wavedir = ['../waveforms/'];
 waveform = [wavedir,'chirp50.bin'];
 
+% ### Apply Amplitude Correction to each pulse individually ###
 correct_amplitude =0;
-chirp_filter=1;
+% ### Apply H : wideband-chirp inversion filter calculated from ref  ###
+chirp_filter=0;
+% ### Set Hz : wideband-chirp inversion filter calculated from loopback data  ###
+is_ref_filt = 0;
+% ### Apply ref filter H or apply previously calculated Hz
+use_ref_as_filt = 0;
 
 trials = udar_read(outdir);
+
 filt = file2wave(waveform);
 
 trials = trials(([trials.freq]>=fmin)&([trials.freq]<=fmax));
+
 
 zp_pre = 512;
 filt = filt(zp_pre+1:end);
 lb_rxAscale=[];
 lb_rxBscale=[];
+
+reftrials = trials;
 
 for i=1:numel(trials)
     refscale = double(intmax('int16'))/max([max(real(trials(i).ref)),max(imag(trials(i).ref))]);
@@ -38,15 +48,16 @@ for i=1:numel(trials)
     trials(i).ref = refscale*trials(i).ref(zp_pre+1:end);
     
     trials(i).awglen = trials(i).awglen-zp_pre;
-
+    
     % Correct Phase
     [d,l]=xcorr(trials(i).ref,filt);
     len = 2*numel(trials(i).ref);
     dfft = fftshift(fft(ifftshift(d),len));
     xfft = fftshift(fft(trials(i).ref,len));
     zfft = fftshift(fft(trials(i).data,len));
-    xfft = xfft.*exp(-1i*angle(dfft));
-    zfft = zfft.*exp(-1i*angle(dfft));
+    
+%     xfft = xfft.*exp(-1i*angle(dfft));
+%     zfft = zfft.*exp(-1i*angle(dfft));
   
     
     % Correct Amplitude
@@ -75,40 +86,50 @@ for i=1:numel(trials)
     filtmp=[filt,zeros(1,numel(trials(i).ref)-numel(filt))];
 %     trials(i).ref = filtmp;
 
+    reftrials(i).data = trials(i).ref;
+    reftrials(i).ref = trials(i).ref;
+    reftrials(i).awglen = trials(i).awglen;
+
 end
 
-
 trials = trials(1:1:end);
-[z,x,x_tx,upfac]=freqstack5(trials,48e6,5e7, ceil(numel(trials)*5e7/fs));
+
+upfac = ceil(numel(trials)*5e7/fs);
+%%
+[z,x,x_tx,upfac]=freqstack5(trials,48e6,5e7, upfac);
+
 % [z,x,x_tx,upfac]=freqstack6(trials,dfc,Bs, ceil(numel(trials)*5e7/fs));
 % [z,x,~,upfac]=freqstack_num(trials,dfc,Bs, ceil(numel(trials)*5e7/fs));
 
 figure;
-plotanalysis(z,[],'freq','zero','db','z spectrum');
-%figure;  hold on;
+obw(z,fs*upfac); title('z spectrum');
 figure;
-plotanalysis(x,[],'freq','zero','db','x spectrum');
-
+obw(x,fs*upfac); title('x spectrum');
+ 
 if(chirp_filter)
 zfft = fft(z);
 xfft = fft(x);
 x_txfft = fft(x_tx);
 H = (x_txfft./xfft);
 H(~isfinite(H))=1;
-% Hz = (x_txfft./zfft);
-% Hz(~isfinite(Hz))=1;
-zfft = zfft.*Hz;
+if (is_ref_filt)
+    Hz = (x_txfft./zfft);
+    Hz(~isfinite(Hz))=1;
+end
+if (use_ref_as_filt)
+    zfft = zfft.*Hz;
+else
+    zfft = zfft.*H;
+end
 xfft = xfft.*H;
 x = ifft(xfft);
 z = ifft(zfft);
 figure;
-plotanalysis(x_tx,[],'freq','zero','db','x_tx spectrum');
-
+obw(x_tx,fs*upfac); title('x_tx spectrum');
 figure;
-plotanalysis(z,[],'freq','zero','db','z spectrum - chirp filter');
-%figure;  hold on;
+obw(z,fs*upfac); title('z spectrum - chirp corrected');
 figure;
-plotanalysis(x,[],'freq','zero','db','x spectrum - chirp filter');
+obw(x,fs*upfac); title('x spectrum - chirp corrected');
 
 end
 
@@ -133,11 +154,21 @@ if (chirp_filter)
 end
 
 figure;
-cplot(imrng,imdata); grid on; axis tight;
+plot(imrng,imdata); grid on; axis tight;
 xlabel('Range(m)'); ylabel('dB'); title(trialstr);
 
 figure(100); hold on;
-[~,~,~,lstrs] = legend();
+if verLessThan('matlab','8.4') % 2014a and earlier
+    [~,~,~,lstrs] = legend();
+else
+    LEGH = legend;
+    if (numel(LEGH)>0)
+        lstrs = LEGH.String;
+    else
+        lstrs = {};
+    end
+end
+
 if (numel(lstrs)>0)
     legstr = {lstrs{:},[sprintf('Test %i: ',numel(lstrs)+1),trialstr]};
 else
@@ -145,13 +176,106 @@ else
     scale = max(imdata);
 end
 
-shift = scale-max(imdata);
-imdata = imdata+shift;
+ashift = scale-max(imdata);
+imdata = imdata+ashift;
 
-cplot(imrng,imdata); grid on; axis tight;
+plot(imrng,imdata); grid on; axis tight;
 xlabel('Range(m)'); ylabel('dB');
 legend(legstr); title(sprintf('Stacked Frequencies: %g-%g mhz',fmin/1e6,fmax/1e6));
 
+%%
+% ### Apply H : wideband-chirp inversion filter calculated from ref  ###
+chirp_filter_sc=0;
+% ### Set Hz : wideband-chirp inversion filter calculated from loopback data  ###
+is_ref_filt_sc = 0;
+% ### Apply ref filter H or apply previously calculated Hz
+use_ref_as_filt_sc = 1;
+% ### Apply Ideal Chirp Filter
+ideal_filter_sc = 1;
+
+[d_sc,l_sc,d_sc_tx,upfac_sc]=freqstack_compress(trials,48e6,50e6, upfac);
+
+[dref_sc,lref_sc,dref_sc_tx,upfacref_sc]=freqstack_compress(reftrials,48e6,50e6, upfac);
+
+
+figure;
+subplot(2,1,1); plot(abs(fftshift(fft(ifftshift(d_sc))))); title('mag fft xcorr');
+subplot(2,1,2); plot(unwrap(angle(fftshift(fft(ifftshift(d_sc)))))); title('phase fft xcorr');
+
+figure;
+subplot(2,1,1); plot(abs(fftshift(fft(ifftshift(d_sc_tx))))); title('ideal mag fft xcorr');
+subplot(2,1,2); plot(unwrap(angle(fftshift(fft(ifftshift(d_sc_tx)))))); title('ideal phase fft xcorr');
+
+if(chirp_filter_sc)
+    dfft = fft(d_sc);
+    if (is_ref_filt_sc)
+        Gz = (fft(d_sc_tx)./dfft);
+        Gz(~isfinite(Gz))=1;
+    end
+    if (use_ref_as_filt_sc)
+        dfft = dfft.*Gz;
+    end
+    d_sc = ifft(dfft);
+    figure;
+    subplot(2,1,1); plot(abs(fftshift(fft(ifftshift(d_sc))))); title('d_sc corrected - mag fft xcorr');
+    subplot(2,1,2); plot(unwrap(angle(fftshift(fft(ifftshift(d_sc)))))); title('d_sc corrected - phase fft xcorr');
+elseif(ideal_filter_sc)
+    dfft = fft(d_sc);
+    H = fft(dref_sc_tx)./fft(dref_sc);
+    % divide by DC gain for unitary filter gain
+    H = H./abs(H(1));
+    dfft = dfft.*H;
+    d_sc = ifft(dfft);
+    figure;
+    subplot(2,1,1); plot(abs(fftshift(fft(ifftshift(d_sc))))); title('d_sc ideal filt corrected - mag fft xcorr');
+    subplot(2,1,2); plot(unwrap(angle(fftshift(fft(ifftshift(d_sc)))))); title('d_sc ideal filt corrected - phase fft xcorr');
+end
+
+figure; hold on;
+plot(l_sc,20*log10(abs(d_sc)));
+plot(l_sc,20*log10(abs(d_sc_tx)));
+figure; hold on;
+plot(interp(l_sc,10),20*log10(abs(interp(d_sc,10))));
+plot(interp(l_sc,10),20*log10(abs(interp(d_sc_tx,10))));
+
+rng = (l_sc/(fs*upfac))*v/2;
+[~,imin]=min(abs(rng-rngint(1)));
+[~,imax]=min(abs(rng-rngint(end)));
+imrng = rng(imin:imax);
+imdata = db(abs(d_sc(imin:imax)),'power');
+
+trialstr = strrep(trialname,'_',' ');
+trialstr = [trialstr,' - freqstack compress'];
+
+figure;
+plot(imrng,imdata); grid on; axis tight;
+xlabel('Range(m)'); ylabel('dB'); title(trialstr);
+
+figure(100); hold on;
+if verLessThan('matlab','8.4') % 2014a and earlier
+    [~,~,~,lstrs] = legend();
+else
+    LEGH = legend;
+    if (numel(LEGH)>0)
+        lstrs = LEGH.String;
+    else
+        lstrs = {};
+    end
+end
+
+if (numel(lstrs)>0)
+    legstr = {lstrs{:},[sprintf('Test %i: ',numel(lstrs)+1),trialstr]};
+else
+    legstr = {['Test 1: ',trialstr]};
+    scale = max(imdata);
+end
+
+ashift = scale-max(imdata);
+imdata = imdata+ashift;
+
+plot(imrng,imdata); grid on; axis tight;
+xlabel('Range(m)'); ylabel('dB');
+legend(legstr); title(sprintf('Stacked Frequencies: %g-%g mhz',fmin/1e6,fmax/1e6));
 %%
 rxAs = lb_rxAscale.';
 rxAs = rxAs(:).';
