67a734780bdd31d0997ff734fff64d179c6b8ee3
Samuel Prager <prager@LMC-029543.jpl.nasa.gov>
2019-04-16 12:14:21 -0700

    fix typos in doptrack

---
+++ host/include/uhd/rfnoc/dopplertracker_block_ctrl.hpp
@@ -0,0 +1,58 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#ifndef INCLUDED_LIBUHD_RFNOC_DOPPLERTRACKER_BLOCK_CTRL_HPP
+#define INCLUDED_LIBUHD_RFNOC_DOPPLERTRACKER_BLOCK_CTRL_HPP
+
+#include <uhd/rfnoc/source_block_ctrl_base.hpp>
+#include <uhd/rfnoc/sink_block_ctrl_base.hpp>
+
+namespace uhd {
+    namespace rfnoc {
+
+/*! \brief Block controller for the standard copy RFNoC block.
+ *
+ */
+class UHD_RFNOC_API dopplertracker_block_ctrl : public source_block_ctrl_base, public sink_block_ctrl_base
+{
+public:
+    UHD_RFNOC_BLOCK_OBJECT(dopplertracker_block_ctrl)
+
+    /*!
+     * Your block configuration here
+    */
+    virtual void set_mavg_len(uint32_t val) = 0;
+    virtual void set_zc_threshold(int16_t i_val,int16_t q_val) = 0;
+    virtual void set_zc_threshold(uint32_t val) = 0;
+    virtual void set_zc_offset(int16_t i_val,int16_t q_val) = 0;
+    virtual void set_zc_offset(int32_t val) = 0;
+    virtual void run_calibration(uint32_t len) = 0;
+    virtual void set_zcsum_len(uint32_t val) = 0;
+    virtual uint64_t get_mavg_len() = 0;
+    virtual uint64_t get_mavg_div() = 0;
+    virtual uint64_t get_cycles_per_sec() = 0;
+    virtual uint64_t get_zc_count() = 0;
+
+}; /* class dopplertracker_block_ctrl*/
+
+}} /* namespace uhd::rfnoc */
+
+#endif /* INCLUDED_LIBUHD_RFNOC_DOPPLERTRACKER_BLOCK_CTRL_HPP */
---
+++ host/lib/rfnoc/dopplertracker_block_ctrl_impl.cpp
@@ -0,0 +1,140 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#include <uhd/rfnoc/dopplertracker_block_ctrl.hpp>
+#include <uhd/convert.hpp>
+#include <uhd/utils/log.hpp>
+#include <uhd/types/ranges.hpp>
+#include <uhd/types/direction.hpp>
+#include <uhd/types/stream_cmd.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/bind.hpp>
+// #include <boost/random.hpp>
+// #include <boost/random/random_device.hpp>
+#include <random>
+#include <math.h>
+
+using namespace uhd;
+using namespace uhd::rfnoc;
+
+
+class dopplertracker_block_ctrl_impl : public dopplertracker_block_ctrl
+{
+public:
+    static const boost::uint32_t SR_SUM_LEN = 192;
+    static const boost::uint32_t SR_DIVISOR = 193;
+    static const boost::uint32_t SR_THRESHOLD = 194;
+    static const boost::uint32_t SR_OFFSET = 195;
+    static const boost::uint32_t SR_CALIBRATE = 196;
+    static const boost::uint32_t SR_ZC_SUM_LEN = 197;
+
+    /* Timekeeper readback registers */
+    static const boost::uint32_t RB_SUM_LEN              = 0;
+    static const boost::uint32_t RB_DIVISOR         = 1;
+    static const boost::uint32_t RB_CYCLES_PER_SEC  = 2;
+    static const boost::uint32_t RB_ZC                 = 3;
+
+
+    static const boost::uint32_t DEFAULT_SPP = 64;
+
+    UHD_RFNOC_BLOCK_CONSTRUCTOR(dopplertracker_block_ctrl),
+        _item_type("sc16") // We only support sc16 in this block
+    {
+        set_mavg_len(1);
+        set_zcsum_len(1);
+    }
+    void set_mavg_len(uint32_t val)
+    {
+      if (val > 255) {
+          throw uhd::value_error(str(
+              boost::format("dopplertracker_block: Requested moving avg length %d is greater than max allowed %d.\n")
+              % val % 255
+          ));
+      }
+      UHD_RFNOC_BLOCK_TRACE() << "dopplertracker_block::set_mavg_len()" << std::endl;
+      sr_write(SR_SUM_LEN, val);
+      sr_write(SR_DIVISOR, val);
+    }
+    void set_zc_threshold(int16_t i_val,int16_t q_val)
+    {
+      uint32_t iq_val = ((0x0000FFFF & i_val) << 16) + (0x0000FFFF & q_val);
+      set_zc_threshold(iq_val);
+    }
+    void set_zc_threshold(uint32_t val)
+    {
+      sr_write(SR_THRESHOLD, val);
+    }
+    void set_zc_offset(int16_t i_val,int16_t q_val)
+    {
+      uint32_t iq_val = ((0x0000FFFF & i_val) << 16) + (0x0000FFFF & q_val);
+      set_zc_offset(iq_val);
+    }
+    void set_zc_offset(int32_t val)
+    {
+      sr_write(SR_OFFSET, val);
+    }
+    void run_calibration(uint32_t len)
+    {
+      sr_write(SR_CALIBRATE, len);
+    }
+    void set_zcsum_len(uint32_t val)
+    {
+      if (val > 255) {
+          throw uhd::value_error(str(
+              boost::format("dopplertracker_block: Requested zc avg length %d is greater than max allowed %d.\n")
+              % val % 255
+          ));
+      }
+      UHD_RFNOC_BLOCK_TRACE() << "dopplertracker_block::set_zcsum_len()" << std::endl;
+      sr_write(SR_ZC_SUM_LEN, val);
+    }
+    uint64_t get_mavg_len()
+    {
+      uint64_t val = boost::uint64_t(user_reg_read64(RB_SUM_LEN));
+      return val;
+    }
+    uint64_t get_mavg_div()
+    {
+      uint64_t val = boost::uint64_t(user_reg_read64(RB_DIVISOR));
+      return val;
+    }
+    uint64_t get_cycles_per_sec()
+    {
+      uint64_t val = boost::uint64_t(user_reg_read64(RB_CYCLES_PER_SEC));
+      return val;
+    }
+    uint64_t get_zc_count()
+    {
+      uint64_t val = boost::uint64_t(user_reg_read64(RB_ZC));
+      return val;
+    }
+
+
+
+
+
+private:
+    const std::string _item_type;
+    double _tick_rate;
+    int _spp;
+};
+
+UHD_RFNOC_BLOCK_REGISTER(dopplertracker_block_ctrl,"dopplertracker");
